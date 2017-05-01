91ce6a2f2931cef663cae661fbbdc449f2cb7ba8
MrElektronix <milovanpelt@gmail.com>
2017-05-01 18:17:19 +0200

    Scene
    - inititialize your three js scene

--- library/simplify.js
+++ library/simplify.js
@@ -3,5 +3,20 @@ class simplify {
 
   }
 
+  initScene() {
+    let container;
+    let SceneWidth;
+    let SceneHeight;
 
+    function CalculateSceneSize() {
+      container = document.getElementById("container");
+      SceneWidth = container.getBoundingClientRect().width;
+      SceneHeight = container.getBoundingClientRect().height;
+    }
+    CalculateSceneSize();
+    this.scene = new THREE.Scene();
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(SceneWidth.SceneHeight);
+    container.appendChild(this.renderer.domElement);
+  }
 }
